fb7c42dca6b6b31817eef515583f2117bc907cdd
Łukasz Czyż <krawacik3@gmail.com>
2021-12-04 23:01:43 +0100

    Day 3 Star 2

--- day_3.cpp
+++ day_3.cpp
@@ -3,12 +3,12 @@
 #include <bitset>
 #include <array>
 #include <iostream>
+#include <numeric>
 
 
 template<size_t numOfBits>
 void partOne(const auto &input) {
   using BitSet = std::bitset<numOfBits>;
-//  using BitSetVector = std::vector<BitSet>;
   using BitCountArray = std::array<int, numOfBits>;
   // Assume true adds 1 to result value, false subtracts 1
   BitCountArray bitCount{};
@@ -33,9 +33,57 @@ void partOne(const auto &input) {
   std::cout << "Gamma: " << gamma << ", epsilon: " << epsilon << ", multiplied: " << gamma * epsilon << std::endl;
 }
 
+template<size_t numOfBits>
+void partTwo(const auto &input) {
+  using BitSet = std::bitset<numOfBits>;
+  using BitSetVector = std::vector<BitSet>;
+
+  BitSetVector bitSetVector{};
+
+  // Convert string input to vector of bitsets
+  for (const auto&[line]: input) {
+    bitSetVector.emplace_back(BitSet(line));
+  }
+
+  // Calculate collection matching requirements. First requirement is operator (most common bit - std::greater or least common - std::less), second is value that is being counted
+  static const auto determineNextBitSetCollection = []<typename UnaryOp>(const BitSetVector& inputCollection, const size_t bitPosition) -> BitSetVector{
+    BitSetVector result{};
+    // Invert position (bitPostition == 0 -> MSB)
+    const size_t invertedBitPosition = BitSet{}.size() - bitPosition - 1;
+    const auto numOfCountedValue = std::accumulate(inputCollection.cbegin(), inputCollection.cend(), 0, [invertedBitPosition](const int result, const BitSet& input) {
+      return result + (input.test(invertedBitPosition)? 1 : -1);
+    });
+
+    const bool bitsToSave = UnaryOp{}(numOfCountedValue, 0);
+    std::cout << "numOfCountedValue: " << numOfCountedValue << ", bitsToSave: " << bitsToSave << std::endl;
+    for(const auto& input : inputCollection) {
+      if (input.test(invertedBitPosition) == bitsToSave) [[likely]] {
+        result.push_back(input);
+      }
+    }
+
+    return result;
+  };
+
+  static const auto determineRating = [&bitSetVector]<typename UnaryOp>() -> auto{
+    auto collectionForIteration = bitSetVector;
+    for(size_t idx = 0; idx < numOfBits && collectionForIteration.size() > 1; ++idx) {
+      collectionForIteration = determineNextBitSetCollection.template operator()<UnaryOp>(collectionForIteration, idx);
+      std::cout << "Collection size: " << collectionForIteration.size() << std::endl;
+    }
+
+    return collectionForIteration.cbegin()->to_ulong();
+  };
+
+  const auto oxygenRating = determineRating.template operator()<std::greater_equal<>>();
+  const auto co2Rating = determineRating.template operator()<std::less<>>();
+
+  std::cout << "Result for Oxygen: " <<  oxygenRating << ", CO2: " << co2Rating << ", multiplying gives: " << oxygenRating * co2Rating << std::endl;
+}
+
 int main() {
   static constexpr auto inputFile = R"(C:\Code\aoc_2021\input\day_3.txt)";
   const auto inputData = readFromFile<std::string>(inputFile);
 
-  partOne<12>(inputData);
+  partTwo<12>(inputData);
 }
