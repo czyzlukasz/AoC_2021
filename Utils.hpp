2a355ecddb6ae93ee4c12303bed1e95060cd7ad8
Łukasz Czyż <krawacik3@gmail.com>
2021-12-05 19:07:01 +0100

    Fixed order of argument evaluation for loading data from file

--- Utils.hpp
+++ Utils.hpp
@@ -9,11 +9,11 @@
 
 template<typename... Format>
 std::vector<std::tuple<Format...>> readFromFile(const std::string_view& input_file) {
-  std::ifstream input_file_stream(input_file.data(), std::ios::in);
+  std::ifstream inputFileStream(input_file.data(), std::ios::in);
   std::vector<std::tuple<Format...>> result{};
 
-  while(not input_file_stream.eof()) {
-    result.emplace_back(std::make_tuple(*std::istream_iterator<std::decay_t<Format>>(input_file_stream)...));
+  while(not inputFileStream.eof()) {
+    result.emplace_back(std::tuple<Format...>{*std::istream_iterator<std::decay_t<Format>>(inputFileStream)...});
   }
 
   return result;
