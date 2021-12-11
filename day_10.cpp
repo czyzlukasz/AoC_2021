4f8d84326dc6d5ae4aec152ab48ac2c91dbbdd5f
Łukasz Czyż <krawacik3@gmail.com>
2021-12-11 20:48:38 +0100

    Day 10 Star 2 Unfinished

--- day_10.cpp
+++ day_10.cpp
@@ -26,19 +26,26 @@ struct Chunk {
   explicit Chunk(const ChunkType chunkType, ChunkCharacter begin, ChunkCharacter end)
           : chunkType(chunkType), begin(begin), end(end) {};
 
+  bool tryToRemoveValidChunkFromLine(std::string& line) const {
+    const std::string validChunk{static_cast<char>(begin), static_cast<char>(end)};
+
+    if(const auto it = line.find(validChunk); it != std::string::npos) {
+      line.erase(it, validChunk.size());
+      return true;
+    }
+
+    return false;
+  }
+
   const ChunkType chunkType;
   const ChunkCharacter begin;
   const ChunkCharacter end;
 
-  static const inline std::map<ChunkCharacter, unsigned> characterToScore{
-          {ChunkCharacter::TRIANGLE_BEGIN, 25137},
-          {ChunkCharacter::TRIANGLE_END,   25137},
-          {ChunkCharacter::SQUARE_BEGIN,   57},
-          {ChunkCharacter::SQUARE_END,     57},
-          {ChunkCharacter::CURLY_BEGIN,    1197},
-          {ChunkCharacter::CURLY_END,      1197},
-          {ChunkCharacter::ROUND_BEGIN,    3},
-          {ChunkCharacter::ROUND_END,      3}
+  static const inline std::map<ChunkCharacter, unsigned long> characterToScore{
+          {ChunkCharacter::ROUND_BEGIN,      1},
+          {ChunkCharacter::SQUARE_BEGIN,     2},
+          {ChunkCharacter::CURLY_BEGIN,      3},
+          {ChunkCharacter::TRIANGLE_BEGIN,   4},
   };
 };
 
@@ -56,14 +63,15 @@ int main() {
   static constexpr auto inputFile = R"(C:\Code\aoc_2021\input\day_10.txt)";
   const auto inputData = readFromFile<std::string>(inputFile);
 
-  unsigned score = 0;
+  std::vector<unsigned long> score;
+
+  std::vector<Chunk> possibleChunks{
+          Chunk(ChunkType::ROUND, ChunkCharacter::ROUND_BEGIN, ChunkCharacter::ROUND_END),
+          Chunk(ChunkType::SQUARE, ChunkCharacter::SQUARE_BEGIN, ChunkCharacter::SQUARE_END),
+          Chunk(ChunkType::CURLY, ChunkCharacter::CURLY_BEGIN, ChunkCharacter::CURLY_END),
+          Chunk(ChunkType::TRIANGLE, ChunkCharacter::TRIANGLE_BEGIN, ChunkCharacter::TRIANGLE_END)};
 
   for (const auto&[inputLine]: inputData) {
-    std::vector<Chunk> possibleChunks{
-      Chunk(ChunkType::ROUND, ChunkCharacter::ROUND_BEGIN, ChunkCharacter::ROUND_END),
-      Chunk(ChunkType::SQUARE, ChunkCharacter::SQUARE_BEGIN, ChunkCharacter::SQUARE_END),
-      Chunk(ChunkType::CURLY, ChunkCharacter::CURLY_BEGIN, ChunkCharacter::CURLY_END),
-      Chunk(ChunkType::TRIANGLE, ChunkCharacter::TRIANGLE_BEGIN, ChunkCharacter::TRIANGLE_END)};
 
     std::stack<ChunkType> chunkStack;
 
@@ -75,8 +83,8 @@ int main() {
         }
         else if(chunk.end == static_cast<ChunkCharacter>(character)){
           if(chunkStack.top() != chunk.chunkType) {
-            std::cout << "Corrupted: " << character << std::endl;
-            score += Chunk::characterToScore.at(static_cast<ChunkCharacter>(character));
+//            std::cout << "Corrupted: " << character << std::endl;
+//            score += Chunk::characterToScore.at(static_cast<ChunkCharacter>(character));
             lineCorrupted = true;
             break;
           }
@@ -89,7 +97,39 @@ int main() {
         break;
       }
     }
+
+    if(not chunkStack.empty()) {
+      std::cout << "OCHUJ";
+    }
+
+    if(not lineCorrupted) {
+      std::string mutableLine = inputLine;
+      bool chunksBeingRemoved = false;
+      do {
+        chunksBeingRemoved = false;
+        for(const Chunk& chunk : possibleChunks) {
+          chunksBeingRemoved |= chunk.tryToRemoveValidChunkFromLine(mutableLine);
+        }
+      } while (chunksBeingRemoved);
+      std::cout << mutableLine << std::endl;
+
+      unsigned long tempScore = 0;
+      std::for_each(mutableLine.rbegin(), mutableLine.rend(), [&tempScore](const char endChar) {
+        tempScore = tempScore * 5 + Chunk::characterToScore.at(static_cast<ChunkCharacter>(endChar));
+      });
+
+      score.push_back(tempScore);
+    }
   }
 
-  std::cout << "Total score: " << score << std::endl;
+  for(const unsigned long x : score) {
+    std::cout << x << std::endl;
+  }
+  std::sort(score.begin(), score.end());
+
+  std::cout << std::endl;
+  for(const unsigned long x : score) {
+    std::cout << x << std::endl;
+  }
+  std::cout << "Middle score: " << score.at((score.size() - 1)  / 2 ) << ". Number of invalid: " << score.size() << std::endl;
 }
