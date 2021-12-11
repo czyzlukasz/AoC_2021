a54781b04c063952e4b1f9ef3cd44e585cd66124
Łukasz Czyż <krawacik3@gmail.com>
2021-12-11 19:07:12 +0100

    Day 9 Star 2

--- day_9.cpp
+++ day_9.cpp
@@ -3,14 +3,18 @@
 #include <algorithm>
 #include <functional>
 #include <array>
+#include <queue>
 
 
-struct Map2D {
+class Map2D {
+  std::vector<std::vector<std::tuple<unsigned, bool, bool>>> map;
+  using ValueType = decltype(map)::value_type::value_type;
+public:
   Map2D(const auto& inputData) {
     for(const auto& [line] : inputData) {
       map.emplace_back();
       for(const char number : line) {
-        map.back().push_back(number - '0');
+        map.back().emplace_back(number - '0', number == '9', false);
       }
     }
   }
@@ -20,16 +24,20 @@ struct Map2D {
     return {map.back().size(), map.size()};
   };
 
-  const unsigned at(size_t x, size_t y) const{
+  const ValueType& at(size_t x, size_t y) const{
     return map.at(y).at(x);
   }
 
-  std::array<unsigned, 4> getAdjacent(size_t x, size_t y) const {
+  ValueType& at(size_t x, size_t y){
+    return map.at(y).at(x);
+  }
+
+  std::array<ValueType , 4> getAdjacent(size_t x, size_t y) const {
     // I know thats bad
-    unsigned up = std::numeric_limits<unsigned>::max();
-    unsigned right = std::numeric_limits<unsigned>::max();
-    unsigned down = std::numeric_limits<unsigned>::max();
-    unsigned left = std::numeric_limits<unsigned>::max();
+    ValueType up = {std::numeric_limits<unsigned>::max(), true, true};
+    ValueType right = {std::numeric_limits<unsigned>::max(), true, true};
+    ValueType down = {std::numeric_limits<unsigned>::max(), true, true};
+    ValueType left = {std::numeric_limits<unsigned>::max(), true, true};
 
     const auto [sizeX, sizeY] = getSize();
     if(sizeY > y + 1) {
@@ -48,8 +56,6 @@ struct Map2D {
     return {up, right, down, left};
   }
 
-private:
-  std::vector<std::vector<unsigned>> map;
 };
 
 int main() {
@@ -57,23 +63,54 @@ int main() {
   const auto inputData = readFromFile<std::string>(inputFile);
 
   Map2D map2d(inputData);
-
-
-  unsigned result = 0;
+  std::vector<size_t> basinSizes;
 
   const auto [sizeX, sizeY] = map2d.getSize();
 
   for(size_t y = 0; y < sizeY; ++y) {
     for(size_t x = 0; x < sizeX; ++x) {
-      const unsigned value = map2d.at(x, y);
-      const auto adjacent = map2d.getAdjacent(x, y);
-      if(std::all_of(adjacent.begin(), adjacent.end(), [value](unsigned adjacentField){
-        return value < adjacentField;
-      })){
-        result += value + 1;
+      const auto& [value, basinEdge, used] = map2d.at(x, y);
+
+      if(not basinEdge and not used) {
+        struct BasinField {
+          size_t x, y;
+          auto operator <=>(const BasinField& other) const = default;
+        };
+
+        std::vector<BasinField> fieldsVerified;
+        std::queue<BasinField> fieldsToCheck;
+
+        fieldsToCheck.push(BasinField{x, y});
+
+        while(not fieldsToCheck.empty()) {
+          const auto [xToCheck, yToCheck] = fieldsToCheck.front();
+          fieldsToCheck.pop();
+
+          const auto[up, right, down, left] = map2d.getAdjacent(xToCheck, yToCheck);
+          static const auto checkIfFieldIsInBasin = [&fieldsToCheck, &fieldsVerified, &map2d](const auto &field, unsigned x,
+                                                                             unsigned y) {
+            const auto&[value, basinEdge, used] = field;
+            if (not basinEdge and not used) {
+              std::get<2>(map2d.at(x, y)) = true;
+              fieldsVerified.emplace_back(BasinField{x, y});
+              fieldsToCheck.push(BasinField{x, y});
+            }
+          };
+
+          checkIfFieldIsInBasin(up, xToCheck, yToCheck - 1);
+          checkIfFieldIsInBasin(right, xToCheck + 1, yToCheck);
+          checkIfFieldIsInBasin(down, xToCheck, yToCheck + 1);
+          checkIfFieldIsInBasin(left, xToCheck - 1, yToCheck);
+        }
+
+        std::cout << "Basin size: " << fieldsVerified.size() << std::endl;
+        basinSizes.emplace_back(fieldsVerified.size());
       }
     }
   }
-
-  std::cout << "Result is: " << result << std::endl;
+  std::sort(basinSizes.begin(), basinSizes.end());
+  for(const auto basinSize : basinSizes) {
+    std::cout << basinSize << ", ";
+  }
+  std::cout << std::endl;
 }
