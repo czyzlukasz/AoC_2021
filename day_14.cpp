3e54c1e88f4f1e2e0590d49f04c7259a9afdb769
Łukasz Czyż <krawacik3@gmail.com>
2021-12-18 00:45:08 +0100

    Day 14 Star 2

--- day_14.cpp
+++ day_14.cpp
@@ -1,44 +1,110 @@
-#include <list>
+#include <map>
 #include <iostream>
 #include <algorithm>
+#include <array>
 #include "Utils.hpp"
+
+struct PairPattern {
+  PairPattern() = default;
+  PairPattern(const char a, const char b) : pattern{a, b} {}
+
+  bool operator == (const PairPattern& other) const {
+    for(size_t idx = 0; idx < insertionPairSize; ++idx) {
+      if(pattern[idx] != other.pattern[idx]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  bool operator < (const PairPattern& other) const {
+    return pattern[0] == other.pattern[0] ? pattern[1] < other.pattern[1] : pattern[0] < other.pattern[0];
+  }
+
+  static constexpr size_t insertionPairSize = 2;
+  char pattern[insertionPairSize];
+};
 
 struct PairInsertion {
   PairInsertion(const std::string& input) {
     static const size_t separatorSize = strlen(" -> ");
-    std::fill(pattern, pattern + sizeof(pattern), '\0');
-    std::copy(input.cbegin(), input.cbegin() + insertionPairSize, pattern);
-    outputCharacter = input.at(insertionPairSize + separatorSize);
+    std::copy(input.cbegin(), input.cbegin() + PairPattern::insertionPairSize, pairPattern.pattern);
+    outputCharacter = input.at(PairPattern::insertionPairSize + separatorSize);
   }
 
-  static constexpr size_t insertionPairSize = 2;
-  char pattern[insertionPairSize + 1]{};
+  bool matches(const PairPattern& other) const {
+    return pairPattern == other;
+  }
+
+  std::array<PairPattern, 2> generatePairPatternsFromInsertion(const PairPattern& pairPattern_) const{
+    return {PairPattern{pairPattern_.pattern[0], outputCharacter}, {outputCharacter, pairPattern_.pattern[1]}};
+  }
+
+  PairPattern pairPattern{};
   char outputCharacter;
 };
 
-void findPolymer(const std::vector<PairInsertion>& pairInsertion, const auto inputIt, std::insert_iterator<std::list<char>> resultIt) {
-  for(const PairInsertion& pairToMatch : pairInsertion) {
-    const std::string_view polymerPair(inputIt, inputIt + PairInsertion::insertionPairSize);
-    if(polymerPair.compare(0, PairInsertion::insertionPairSize, pairToMatch.pattern) == 0) {
-      resultIt = pairToMatch.outputCharacter;
-      return;
+struct PairContainer {
+  using PatternContainer = std::map<PairPattern, uint64_t>;
+  PairContainer(const std::string& input) {
+    for(auto it = input.cbegin(); it + 1 != input.cend(); ++it) {
+      ++patternFrequency[PairPattern{*it, *(it+1)}];
+    }
+
+    lastPair = PairPattern{*(input.cend() - 2), *(input.cend() - 1)};
+  }
+
+  void iterate(const std::vector<PairInsertion>& pairInsertions){
+    PatternContainer result = patternFrequency;
+    for(const auto& [key, value] : patternFrequency) {
+      for(const PairInsertion& pairInsertion : pairInsertions) {
+        if(pairInsertion.matches(key)) {
+          const auto matchingPairFrequency = patternFrequency[key];
+          result[key] -= matchingPairFrequency;
+          for (const auto &newPair: pairInsertion.generatePairPatternsFromInsertion(key)) {
+            result[newPair] += matchingPairFrequency;
+          }
+        }
+      }
+    }
+
+    // Calculate last pair in order to aid in calculating number of pairs
+    for(const PairInsertion& pairInsertion : pairInsertions) {
+      if(pairInsertion.matches(lastPair)) {
+        lastPair = pairInsertion.generatePairPatternsFromInsertion(lastPair).at(1);
+        break;
+      }
+    }
+    patternFrequency = result;
+  }
+
+  void printPairs() const {
+    for(const auto& [key, value] : patternFrequency) {
+      if(value) {
+        std::cout << key.pattern[0] << key.pattern[1] << ": " << value << std::endl;
+      }
     }
   }
-}
 
-std::string iterate(const std::vector<PairInsertion>& pairInsertion, const std::string& polymerTemplate) {
-  std::list<char> resultPolymerTemplate(polymerTemplate.cbegin(), polymerTemplate.cend());
+  std::map<char, uint64_t> calculateCharacterFrequency() const {
+    std::map<char, uint64_t> result;
 
-  auto resultIt = std::next(resultPolymerTemplate.begin());
-  for(auto inputIt = polymerTemplate.cbegin(); inputIt <= polymerTemplate.cend() - PairInsertion::insertionPairSize; ++inputIt) {
-    findPolymer(pairInsertion, inputIt, std::inserter(resultPolymerTemplate, resultIt));
-    ++resultIt;
+    for(const auto& [key, value] : patternFrequency) {
+      if(value) {
+        result[key.pattern[0]] += value;
+      }
+    }
+
+    ++result[lastPair.pattern[1]];
+    return result;
   }
 
-  return {resultPolymerTemplate.cbegin(), resultPolymerTemplate.cend()};
-}
+  PatternContainer patternFrequency;
+  PairPattern lastPair;
+};
 
 int main() {
+//  static constexpr auto inputFile = R"(C:\Code\aoc_2021\input\test.txt)";
   static constexpr auto inputFile = R"(C:\Code\aoc_2021\input\day_14.txt)";
   const auto inputData = readLineByLine(inputFile);
 
@@ -47,25 +113,21 @@ int main() {
   std::vector<PairInsertion> pairInsertion;
   std::copy(inputData.cbegin() + 2, inputData.cend(), std::back_inserter(pairInsertion));
 
-  std::string result = polymerTemplate;
-  for(size_t idx = 1; idx <= 10; ++idx) {
+  PairContainer pairContainer(polymerTemplate);
+
+  for(uint64_t idx = 1; idx <= 40; ++idx) {
     std::cout << "Step " << idx << std::endl;
-    result = iterate(pairInsertion, result);
+    pairContainer.iterate(pairInsertion);
   }
+  std::cout << "Pairs: " << pairContainer.patternFrequency.size() << std::endl;
+  pairContainer.printPairs();
+  const auto resultMap = pairContainer.calculateCharacterFrequency();
+  std::vector<std::pair<char, uint64_t>> result;
+  std::copy(resultMap.cbegin(), resultMap.cend(), std::back_inserter(result));
 
-  std::cout << "Result has length of " << result.size() << std::endl;
-
-  std::vector<std::pair<char, size_t>> numOfOccurrences;
-
-  for(char character = 'A'; character <= 'Z'; ++character) {
-    numOfOccurrences.emplace_back(character, std::count(result.cbegin(), result.cend(), character));
+  std::sort(result.begin(), result.end(), [](const auto& a, const auto& b){return a.second < b.second;});
+  for(const auto& [key, value] : result) {
+      std::cout << key << ": " << value << std::endl;
   }
-
-  std::sort(numOfOccurrences.begin(), numOfOccurrences.end(), [](const auto& a, const auto& b) {
-    return a.second < b.second;
-  });
-
-  for(const auto& [character, frequency] : numOfOccurrences) {
-    std::cout << character << ": " << frequency << std::endl;
-  }
+  std::cout << "Difference is: " << result.back().second - result.front().second << std::endl;
 }
